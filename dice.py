0ff944495c9d70a70820347eebd53e438253bc67
Coffey <lucascoffey@gmail.com>
2020-04-26 10:55:12 -0400

    dice

--- dice.py
+++ dice.py
@@ -1,10 +1,15 @@
 # Simulation of rolling dice
 
+
+# import random = bringing in or importing the random module into your application
 import random
 
+
+# random.random is
 prob = random.random()
 print(prob)
 
+# rand range is just the range in which the dice can land
 rollDice = random.randrange(1,7)
 print(rollDice)
 
